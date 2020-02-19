85dc3063a748c65fa3a48c18289c87286a25e4a4
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-19 18:06:13 +0000

    Add recipe_batches q

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,25 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # 1: make sure we have all ingredients, regardless of quantity
+  # ie. if recipe dictionary has key 'cheese' and ingredients doesn't, return 0
+  if recipe.keys() != ingredients.keys():
+    return 0
+  
+  # 2: for every ingredient in the recipe (for every key in recipe dict), 
+  # divide the quantity we have of that ingredient by the quantity needed in the recipe
+  # eg. key 'milk' in recipe dict has value of 100 and key 'milk' in ingredients dict has value of 200 ->
+  #     200 // 100 = 2 (using floor division operator '//' to round down)
+  # save result in 'max_batches' list containing max number of batches that can be made for each ingredient
+  max_batches = []
+  
+  for ing in recipe:
+    max_batches.append(ingredients[ing] // recipe[ing])
+  
+  # 3: return the minimum value in the max_batches array
+  # (if the maximum no of batches we could make with the milk we have is 500 but the maximum no. of batches we could 
+  # make with the butter we have is only 10, we're still only going to be able to make 10 batches)
+  return min(max_batches)
 
 
 if __name__ == '__main__':
