fb4cfbe9709e65f91c66893061bf92fbe22e87c0
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-19 16:23:27 +0000

    Refactor - all tests passing

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,37 +3,38 @@
 import argparse
 
 def find_max_profit(prices):
-  # initialize index of max element in array to 0
-  max_index = 0
+  # initialize index of max element in array to 1
+  max_index = 1
+  # initialize index of min element in array to 0
+  min_value = 0
   # initialize max profit to 0
   max_profit = 0
 
 # 1:
-# get index of maximum value from index 0 - index of last element in array
-  for i in range(len(prices)):
+# get index of maximum value from index 1 - index of last element in array
+
+# (if index of maximum value was at index 0, you would have to get the next largest element 
+# after index 0 anyway and calculate max profit from there
+# eg array = [100, 55, 4, 98, 10, 18, 90, 95, 43]
+# max value is 100, at index 0. 
+# To get max profit you would have to find the next largest number (which is 98) 
+# and subtract next min value from it (98 - 4 = 4)
+# Therefore no need to loop over array from index 0, just start at index 1)
+
+  for i in range(1, len(prices)):
     if prices[i] > prices[max_index]:
       max_index = i
 
-#2:
-# once we have the index of the maximum element:
-# if the index of the max element is 0 (it's the first element of the array)
-# the maximum profit will be calculated by getting the next largest number in the array 
-# and subtracting the value of the smallest element before it
-# eg for array [100, 90, 80, 50, 20, 10]
-# max element = 100
-# max index = 0
-# next largest element from index 1 to the last element in array = 90
-# max profit = 90 - 100 = -10
-  if max_index == 0: 
-    next_biggest_value = max(prices[1:])
-    max_profit = next_biggest_value - min(prices[:next_biggest_value])
+# 2  
+# get the value of the smallest element in array up to index of max element
+  min_value = min(prices[:max_index]) 
 
-# otherwise max profit will be calculated by subtracting the smallest element 
-# that comes before index of max element from the max element
-  else:
-    max_profit = prices[max_index] - min(prices[:max_index]) 
-  
-  # return the max profit
+# 3
+# calculate max_profit by subtracting minimum value from value at max_index
+  max_profit = prices[max_index] - min_value 
+
+# 4  
+# return the max profit
   return max_profit
 
 if __name__ == '__main__':
