1c8945d6318aaa50350ee8d34c9199255e4a2cb4
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-20 18:48:05 +0000

    First pass sol - rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  result = []
+  options = ['rock', 'paper', 'scissors']
+
+  def play(played, n):  
+    # base case: if n = 0, just append the results
+    if n == 0: 
+      result.append([played])
+    else:
+      for i in range(3):
+        play(played + options[i], n - 1)
+
+  play('', n)
+  return result
+
+print(rock_paper_scissors(2))  
+
 
 
 if __name__ == "__main__":
